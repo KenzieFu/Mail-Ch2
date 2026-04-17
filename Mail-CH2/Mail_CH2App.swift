a2dbb1e7bd6c23f5674be82815a95be5c0fbd387
KenzieFu <kenziefubrianto@gmail.com>
2026-04-17 15:32:12 +0800

    Initial Commit

---
+++ Mail-CH2/Mail_CH2App.swift
@@ -0,0 +1,17 @@
+//
+//  Mail_CH2App.swift
+//  Mail-CH2
+//
+//  Created by Kenzie Fubrianto on 17/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Mail_CH2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
