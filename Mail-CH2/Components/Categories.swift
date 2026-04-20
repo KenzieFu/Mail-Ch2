be5b7ffee577fd4d9ca9c4a78e804e5da52fb1b2
Dori <michellealdroino29@gmai.com>
2026-04-20 09:00:31 +0800

    tis inbox page

---
+++ Mail-CH2/Components/Categories.swift
@@ -0,0 +1,57 @@
+//
+//  Categories.swift
+//  Mail-CH2
+//
+//  Created by Michelle Aldorino on 18/04/26.
+//
+
+import SwiftUI
+
+struct Categories: View {
+    
+    @State private var selectedIndex: Int = 0
+    let categories = [
+        ("tray.fill", "All Mail", Color.blue),
+        ("envelope.fill", "Read", Color.green),
+        ("envelope.fill", "Unread", Color.yellow)
+    ]
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: 10) {
+                ForEach(categories.indices, id: \.self) { index in
+                    Button {
+                        selectedIndex = index
+                    } label : {
+                        HStack(spacing: 6) {
+                            Image(systemName: categories[index].0)
+                            
+                            if selectedIndex == index {
+                                Text(categories[index].1)
+                            }
+                        }
+                        .fontWeight(
+                            selectedIndex == index ? .semibold : .regular
+                        )
+                        .padding(.horizontal,
+                                 selectedIndex == index ? 71 : 18)
+                        .padding(.vertical, 12)
+                        .background(
+                            selectedIndex == index ? categories[index].2 : Color.gray.opacity(0.4)
+                        )
+                        .foregroundColor(
+                            selectedIndex == index ? (index == 4 ? .black : .white) : .gray
+                        )
+                        .clipShape(Capsule())
+                    }
+                }
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+
+
+#Preview {
+    Categories()
+}
--- Mail-CH2/Types/mail.swift
+++ Mail-CH2/Types/mail.swift
@@ -4,4 +4,28 @@
 //
 //  Created by Kenzie Fubrianto on 18/04/26.
 //
+import SwiftUI
 
+struct Mail: Identifiable {
+    var id: UUID = UUID()
+    
+    //characteristic
+    var sender: String
+    var subject: String
+    var date: Date
+    var recipient: String
+    var summary: String
+    
+    init(sender: String, subject: String, date: Date, recipient: String, summary: String) {
+        self.sender = sender
+        self.subject = subject
+        self.date = date
+        self.recipient = recipient
+        self.summary = summary
+    }
+    
+    //function
+    func open() {
+        //TBA
+    }
+}
